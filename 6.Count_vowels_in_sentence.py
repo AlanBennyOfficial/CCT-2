66d64fc777e2746da7968492fa5b0f1fc2e33459
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-20 22:16:13 +0530

    Added files via git

---
+++ 6.Count_vowels_in_sentence.py
@@ -0,0 +1,9 @@
+# Write a program that asks for a sentence and counts the number of vowels (a, e, i, o, u) present in it.
+
+sentence = input("Enter a sentence: ") 
+vowels = "aeiouAEIOU" 
+count = 0 
+for char in sentence: 
+    if char in vowels: 
+        count += 1 
+print("Number of vowels:", count)
