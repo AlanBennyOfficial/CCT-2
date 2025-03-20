d4aba6d0aa5de0b77c4527174e27307931a5edd0
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-03-20 22:56:52 +0530

    Added files via git

---
+++ 13.Convert_Temperature.py
@@ -0,0 +1,15 @@
+#  Create a program that converts temperature between Celsius and Fahrenheit based on the user’s choice. 
+
+def convert_temperature(temp, unit): 
+    if unit == 'C': 
+        return (temp * 9/5) + 32 
+    else: 
+        return (temp - 32) * 5/9 
+ 
+temp = float(input("Enter temperature: ")) 
+unit = input("Enter 'C' for Celsius, 'F' for Fahrenheit: ").upper() 
+ 
+if unit not in ['C', 'F']: 
+    print("Invalid unit! Enter C or F.") 
+else: 
+    print("Converted Temperature:", convert_temperature(temp, unit))
